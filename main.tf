35a4574c42b04802205ef6bcdf296764c586a213
Qtalha <quazitalhazuhair84@gmail.com>
2022-10-27 17:13:14 +0530

    added tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "talha" {
+  ami                         = "ami-0f69dd1d0d03ad669"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = "true"
+  availability_zone           = "ap-south-1a"
+  vpc_security_group_ids      = ["sg-07065eb2806da15b7"]
+  key_name                    = "sameer"
+}
