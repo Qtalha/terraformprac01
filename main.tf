f542f7815908845e14e11f9a262223cf0b2fe673
Talha <113745023+Qtalha@users.noreply.github.com>
2022-10-28 12:30:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "talha" {
-  ami                         = "ami-0f69dd1d0d03ad669"
+  ami                         = "ami-08c40ec9ead489470"
   instance_type               = "t2.micro"
   associate_public_ip_address = "true"
   availability_zone           = "us-east-1a"
